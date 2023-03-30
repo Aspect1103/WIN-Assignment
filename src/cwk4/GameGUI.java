06610fd0878dfe1502793731c6022ecda4970b55
Aspect1103 <jack.ashwell1@gmail.com>
2023-03-30 12:28:23 +0100

    Refactored GameGUI. It's ready to be completed.

--- src/cwk4/GameGUI.java
+++ src/cwk4/GameGUI.java
@@ -2,64 +2,88 @@ package cwk4;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
- * Provide a GUI interface for the game
+ * Provides a GUI interface for the game
  *
- * @author A.A.Marczyk
- * @version 20/10/23
+ * @author Klevi, Jack, Luke, Abdulla
+ * @version 30/03/2023
  */
 public class GameGUI {
-    private WIN gp = new SpaceWars("Horatio");
-    private JFrame myFrame = new JFrame("Game GUI");
+    private final WIN game = new SpaceWars("Horatio");
+    private final JFrame frame = new JFrame("Game GUI");
+    private final JTextArea listing = new JTextArea();
+    private final JLabel codeLabel = new JLabel();
+    private final JButton fightBtn = new JButton("Fight");
+    private final JPanel eastPanel = new JPanel();
 
-    private JTextArea listing = new JTextArea();
-    private JLabel codeLabel = new JLabel();
-    private JButton fightBtn = new JButton("Fight");
-    private JPanel eastPanel = new JPanel();
-
+    /**
+     * Constructs a gameGUI object.
+     */
     public GameGUI() {
         makeFrame();
-        makeMenuBar(myFrame);
+        makeMenuBar(frame);
+    }
+
+    /**
+     * Runs the GUI.
+     *
+     * @param args A string array of CLI arguments.
+     */
+    public static void main(String[] args) {
+        new GameGUI();
     }
 
     /**
      * Create the main frame's menu bar.
      */
     private void makeMenuBar(JFrame frame) {
+        // Create a menubar
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
 
-        // create the Forces menu
+        // Create the forces menu
         JMenu forcesMenu = new JMenu("Forces");
         menubar.add(forcesMenu);
 
+        // Add a list of forces menu item to the forces menu
         JMenuItem listForcesItem = new JMenuItem("List All Forces ");
-        listForcesItem.addActionListener(new ListForcesHandler());
-        forcesMenu.add(listForcesItem);
+        listForcesItem.addActionListener(e -> {
+            listing.setVisible(true);
+            listing.setText(game.getAllForces());
+        });
     }
 
     /**
      * Create the Swing frame and its content.
      */
     private void makeFrame() {
-        myFrame.setLayout(new BorderLayout());
-        myFrame.add(listing, BorderLayout.CENTER);
+        // Set up the frame's layout
+        frame.setLayout(new BorderLayout());
+        frame.add(listing, BorderLayout.CENTER);
         listing.setVisible(false);
-        myFrame.add(eastPanel, BorderLayout.EAST);
-        // set panel layout and add components
+
+        // Add the east panel to the frame and add the fight button
+        frame.add(eastPanel, BorderLayout.EAST);
         eastPanel.setLayout(new GridLayout(4, 1));
         eastPanel.add(fightBtn);
-        fightBtn.addActionListener(new FightBtnHandler());
+        fightBtn.addActionListener(e -> {
+            int battleNumber = Integer.parseInt(JOptionPane.showInputDialog("Fight number ?: "));
+            JOptionPane.showMessageDialog(frame, fighting(game.doBattle(battleNumber)));
+        });
         fightBtn.setVisible(true);
 
-        // building is done - arrange the components and show        
-        myFrame.pack();
-        myFrame.setVisible(true);
+        // Arrange the components on the frame and show the GUI
+        frame.pack();
+        frame.setVisible(true);
     }
 
+    /**
+     * Process the result of a battle.
+     *
+     * @param code The result of the battle.
+     * @return A string representation of the result of a battle.
+     */
     private String fighting(int code) {
         switch (code) {
             case 0:
@@ -71,29 +95,6 @@ public class GameGUI {
             case 3:
                 return "fight is lost and admiral completely defeated ";
         }
-        return " no such fight ";
-    }
-
-    private class ListForcesHandler implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            listing.setVisible(true);
-            String xx = gp.getAllForces();
-            listing.setText(xx);
-
-        }
-    }
-
-    private class FightBtnHandler implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            int result = -1;
-            String inputValue = JOptionPane.showInputDialog("Fight number ?: ");
-            int num = Integer.parseInt(inputValue);
-            result = gp.doBattle(num);
-            JOptionPane.showMessageDialog(myFrame, fighting(result));
-        }
-    }
-
-    public static void main(String[] args) {
-        new GameGUI();
+        return "No such fight";
     }
 }
