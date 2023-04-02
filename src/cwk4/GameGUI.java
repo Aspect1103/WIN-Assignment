9a5e86e904c30ce54644f3857c5495e06b7c6ab7
Aspect1103 <jack.ashwell1@gmail.com>
2023-04-02 17:20:06 +0100

    Reformatted GameGUI.
    The string methods in SpaceWars now signal that something is returned and not return an empty string.
    
    Optimised a few other things.

--- src/cwk4/GameGUI.java
+++ src/cwk4/GameGUI.java
@@ -7,91 +7,81 @@ import java.awt.*;
  * Provides a GUI interface for the game.
  *
  * @author Klevi, Jack, Luke, Abdulla
- * @version 01/04/2023
+ * @version 02/04/2023
  */
 public class GameGUI {
     private final WIN game = new SpaceWars("Horatio");
     private final JFrame frame = new JFrame("Game GUI");
-    private final JTextArea listing = new JTextArea();
-    private final JPanel listingPanel = new JPanel();
-    private final JButton fightButton = new JButton("Fight");
-    private final JButton ViewState = new JButton("View State");
-    private final JButton clear = new JButton("Clear");
-    private final JMenuItem listASF = new JMenuItem("List ASF");
-    private final JMenuItem activateForce = new JMenuItem("Activate Force");
-    private final JMenuItem recallForce = new JMenuItem("Recall Force");
-    private final JMenuItem listForcesItem = new JMenuItem("List All Forces ");
-    private final JMenuItem listBattles = new JMenuItem("List All Battles ");
     private final JPanel eastPanel = new JPanel();
+    private final JPanel outputPanel = new JPanel();
+    private final JMenuBar menubar = new JMenuBar();
+    private final JMenu forcesMenu = new JMenu("Forces");
+    private final JMenu battlesMenu = new JMenu("Battles");
+    private final JTextArea outputText = new JTextArea();
+    private final JButton clearButton = new JButton("Clear");
+    private final JButton fightButton = new JButton("Fight");
+    private final JButton viewStateButton = new JButton("View State");
+    private final JMenuItem listASFItem = new JMenuItem("List ASF");
+    private final JMenuItem recallForceItem = new JMenuItem("Recall Force");
+    private final JMenuItem listBattlesItem = new JMenuItem("List All Battles ");
+    private final JMenuItem activateForceItem = new JMenuItem("Activate Force");
+    private final JMenuItem listForcesItem = new JMenuItem("List All Forces ");
 
     /**
      * Constructs a game GUI object.
      */
     public GameGUI() {
+        // Set the window size
         frame.setPreferredSize(new Dimension(800, 600));
-        frame.setLocation(500, 200);
+
+        // Create the GUI
         makeFrame();
         makeMenuBar();
-    }
-
-    /**
-     * Runs the GUI.
-     *
-     * @param args A string array of CLI arguments.
-     */
-    public static void main(String[] args) {
-        new GameGUI();
     }
 
     /**
      * Create the Swing frame and its content.
      */
     private void makeFrame() {
-        listingPanel.setLayout(new BoxLayout(listingPanel, BoxLayout.Y_AXIS));
-        listingPanel.setBackground(Color.WHITE);
-        listingPanel.setVisible(true);
-        listingPanel.add(listing);
-
-        // Arrange the components on the frame
-        frame.add(eastPanel, BorderLayout.EAST);
-        frame.add(listingPanel, BorderLayout.WEST);
+        // Create the output panel listing the result from top to bottom
+        outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS));
+        outputPanel.setBackground(Color.WHITE);
+        outputPanel.setVisible(true);
+        outputPanel.add(outputText);
 
         // Set up the east panel
         eastPanel.setLayout(new BoxLayout(eastPanel, BoxLayout.Y_AXIS));
         eastPanel.setBackground(Color.WHITE);
         eastPanel.add(fightButton);
         eastPanel.add(Box.createVerticalStrut(10));
-        eastPanel.add(ViewState);
+        eastPanel.add(viewStateButton);
         eastPanel.add(Box.createVerticalStrut(10));
-        eastPanel.add(clear);
+        eastPanel.add(clearButton);
 
-        // Set up the ViewState button
-        ViewState.addActionListener(e -> {
-            listing.setVisible(true);
-            listing.setText(game.toString());
-        });
-        ViewState.setFont(new Font("Arial", Font.BOLD, 16)); // use a larger and bold font
-        ViewState.setForeground(Color.WHITE); // set the text color to white
-        ViewState.setBackground(Color.BLUE);
+        // Set up the view button
+        viewStateButton.addActionListener(e -> outputText.setText(game.toString()));
+        viewStateButton.setFont(new Font("Arial", Font.BOLD, 16));
+        viewStateButton.setForeground(Color.WHITE);
+        viewStateButton.setBackground(Color.BLUE);
 
-        // Set up the Clear button
-        clear.addActionListener(e -> {
-            listing.setText("");
-        });
-        clear.setFont(new Font("Arial", Font.BOLD, 16)); // use a larger and bold font
-        clear.setForeground(Color.WHITE); // set the text color to white
-        clear.setBackground(Color.BLUE);
+        // Set up the clear button
+        clearButton.addActionListener(e -> outputText.setText(""));
+        clearButton.setFont(new Font("Arial", Font.BOLD, 16));
+        clearButton.setForeground(Color.WHITE);
+        clearButton.setBackground(Color.BLUE);
 
         // Set up the fight button
         fightButton.addActionListener(e -> {
-            int battleNumber = Integer.parseInt(JOptionPane.showInputDialog("Fight number ? "));
+            int battleNumber = Integer.parseInt(JOptionPane.showInputDialog("Battle number: "));
             JOptionPane.showMessageDialog(frame, fighting(game.doBattle(battleNumber)));
         });
-        fightButton.setFont(new Font("Arial", Font.BOLD, 16)); // use a larger and bold font
-        fightButton.setForeground(Color.WHITE); // set the text color to white
+        fightButton.setFont(new Font("Arial", Font.BOLD, 16));
+        fightButton.setForeground(Color.WHITE);
         fightButton.setBackground(Color.BLUE);
 
         // Arrange the components on the frame and show the GUI
+        frame.add(eastPanel, BorderLayout.EAST);
+        frame.add(outputPanel, BorderLayout.WEST);
         frame.pack();
         frame.setVisible(true);
     }
@@ -100,63 +90,42 @@ public class GameGUI {
      * Create the main frame's menu bar.
      */
     private void makeMenuBar() {
-
         // Create a menubar
-        JMenuBar menubar = new JMenuBar();
         menubar.setLayout(new BoxLayout(menubar, BoxLayout.X_AXIS));
         menubar.setVisible(true);
 
-        // Arrange the components on the frame
-        frame.setJMenuBar(menubar);
-
-        // Create the forces menu
-        JMenu forcesMenu = new JMenu("Forces");
-
-        // Create the battles menu
-        JMenu battlesMenu = new JMenu("Battles");
-
         // Arrange the components on the menubar
         menubar.add(forcesMenu);
-        menubar.add(Box.createHorizontalStrut(100)); // Add space of 10 pixels between the buttons
+        menubar.add(Box.createHorizontalStrut(100));
         menubar.add(battlesMenu);
 
-        //Adding menu items for forces and battles
-        recallForce.addActionListener(e -> {
-            String forceNum = JOptionPane.showInputDialog("Force reference ? ");
-            game.recallForce(forceNum);
-            JOptionPane.showMessageDialog(frame, "Force " + forceNum + " recalled.");
-            listing.setVisible(true);
+        // Add the menu items for forces and battles
+        recallForceItem.addActionListener(e -> {
+            String forceRef = JOptionPane.showInputDialog("Force reference: ");
+            game.recallForce(forceRef);
+            JOptionPane.showMessageDialog(frame, "Force " + forceRef + " recalled");
         });
 
-        listForcesItem.addActionListener(e -> {
-            listing.setVisible(true);
-            listing.setText(game.getAllForces());
-        });
-
-        activateForce.addActionListener(e -> {
-            String forceNum = JOptionPane.showInputDialog("Force reference ? ");
+        // Add action listeners for each menu item
+        listForcesItem.addActionListener(e -> outputText.setText(game.getAllForces()));
+        activateForceItem.addActionListener(e -> {
+            String forceNum = JOptionPane.showInputDialog("Force reference: ");
             JOptionPane.showMessageDialog(frame, activatingForce(game.activateForce(forceNum)));
-            listing.setVisible(true);
         });
-
-        listASF.addActionListener(e -> {
-            listing.setVisible(true);
-            listing.setText(game.getASFleet());
-        });
-
-        listBattles.addActionListener(e -> {
-            listing.setVisible(true);
-            listing.setText(game.getAllBattles());
-        });
+        listASFItem.addActionListener(e -> outputText.setText(game.getASFleet()));
+        listBattlesItem.addActionListener(e -> outputText.setText(game.getAllBattles()));
 
         // Arrange the components on the forcesMenu
         forcesMenu.add(listForcesItem);
-        forcesMenu.add(recallForce);
-        forcesMenu.add(activateForce);
-        forcesMenu.add(listASF);
+        forcesMenu.add(recallForceItem);
+        forcesMenu.add(activateForceItem);
+        forcesMenu.add(listASFItem);
 
         // Arrange the components on the battlesMenu
-        battlesMenu.add(listBattles);
+        battlesMenu.add(listBattlesItem);
+
+        // Arrange the components on the frame
+        frame.setJMenuBar(menubar);
     }
 
     /**
@@ -197,5 +166,14 @@ public class GameGUI {
             default:
                 return "No such force";
         }
+    }
+
+    /**
+     * Runs the GUI.
+     *
+     * @param args A string array of CLI arguments.
+     */
+    public static void main(String[] args) {
+        new GameGUI();
     }
 }
